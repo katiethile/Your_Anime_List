d934f5e1a7eaacab2e7d09af5fa283aa840ef2b9
katiethile <lethikatie@gmail.com>
2020-10-16 14:35:32 -0700

    refactored edit view, fixed new view

--- app/controllers/animes_controller.rb
+++ app/controllers/animes_controller.rb
@@ -46,28 +46,6 @@ class AnimesController < ApplicationController
     get '/animes/:id/edit' do 
         if is_logged_in?
         @anime = Anime.find_by_id(params[:id])
-        case @anime.rating
-        when 1
-            @oneselect = "selected"
-        when 2
-            @twoselect = "selected"
-        when 3
-            @threeselect = "selected"
-        when 4
-            @fourselect = "selected"
-        when 5
-            @fiveselect = "selected"
-        when 6
-            @sixselect = "selected"
-        when 7
-            @sevenselect = "selected"
-        when 8
-            @eightselect = "selected"
-        when 9
-            @nineselect = "selected"
-        when 10
-            @tenselect = "selected"
-        end
         erb :'animes/edit'
         else
            redirect to '/' 
