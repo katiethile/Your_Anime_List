f6a58633736b3336c4303df1533b4d1752ec0173
katiethile <lethikatie@gmail.com>
2020-10-15 20:01:09 -0700

    added images to views

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,5 @@
 class User < ActiveRecord::Base 
+    validates_uniqueness_of :username
     has_many :animes 
     has_secure_password
-  
-    def slug 
-      self.username.strip.downcase.gsub(" ", "-")
-    end 
-  
-    def self.find_by_slug(slug) 
-      User.all.find {|user| user.slug == slug}
-    end 
 end 
